31996ecdfafbaf2b7f9432735077971dac64a2a3
lukatchubabria <lukatchubabria@gmail.com>
2021-06-21 21:00:03 +0400

    1 task

--- #_1/index.php
+++ #_1/index.php
@@ -6,7 +6,7 @@ $students = ['ლუკა', 'გიორგი', 'სანდრო', 'ირ
 $time = [2, 6, 3, 2];
 
 foreach ($students as $key => $name) {
-    echo $name . " - " . $time[$key] . " დასწრების საათების რაოდენობა";
+    echo "სტუდენტის სახელი " . $name . " - " . "დასწრების საათების რაოდენობა " . $time[$key] ;
     echo '<br>';
 }
 
@@ -19,14 +19,14 @@ echo '<br>';
 // #2
 
 $studentsDesc = [
-    'luka' => 2,
-    'giorgi' => 6,
-    'sandro' => 3,
-    'irakli' =>2
+    'ლუკა' => 2,
+    'გიორგი' => 6,
+    'სანდრო' => 3,
+    'ირაკლი' =>2
     ];
 
 foreach ($studentsDesc as $personName => $personTime) {
-    echo $personName. " - " . $personTime . " დასწრების საათების რაოდენობა";
+    echo "სტუდენტის სახელი " . $personName. " - " . "დასწრების საათების რაოდენობა " . $personTime ;
     echo '<br>';
 }
 
